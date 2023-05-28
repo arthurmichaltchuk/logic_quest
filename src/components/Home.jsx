0004f3a8395e7fa9e41c99230a96f665913a6b2d
AugustoJr01 <augustovelho2012.avf@gmail.com>
2023-05-28 20:53:05 -0300

    Adicionado Footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import { Col, Container, Row } from "react-bootstrap";
 import logo from '../assets/images/logoLogicQuest.png';
 import Button from 'react-bootstrap/Button';
 import AboutHeader from './AboutHeader.jsx'
+import Footer from './Footer.jsx'
 
 function Home() {
     return (
@@ -31,9 +32,11 @@ function Home() {
                     <p>Desenvolvemos um sistema que integra aluno e professor de uma única maneira e com um único objetivo, ficar no top 1 em nosso quiz, que abrange diversos conteúdos com perguntas variadas, uma maneira intuitiva e legal para aprender, com fotos, vídeos e figuras, para que, independentemente da pessoa consiga estudar do melhor jeito possível.</p>
                     <p>Vale lembrar que o professor pode dar seu feedback sobre o assunto em tempo real, tendo assim uma melhor interatividade com seus alunos, mas não se esqueça, se você acertar todas as questões primeiro o topo do ranking te espera com a maior conquista, parabéns você é um Galo Cinza!!</p>
                     <div class="linha"></div>
-                    <p>Teste</p>
+                    <h1>Quem Somos</h1>
+                    <p>Olá, somos os Vetoriosos, nossa equipe está desenvolvendo um projeto de objetos virtuais de aprendizagem. Escolhemos essa ideia para ter uma dinâmica melhor nas aulas e todos conseguirem aprender de uma forma divertida a programar e ter um bom entendimento do conteúdo no qual temos dificuldade.</p>
                 </div>
             </div>
+            <Footer/>
         </>
     );
 }
