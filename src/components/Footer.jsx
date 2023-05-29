d91f0f4189dfb34c9b3b7809a31cf803063a3045
arthurmichaltchuk <arthurmichaltchuk@uniplaclages.edu.br>
2023-05-29 19:19:23 -0300

    Footer feito

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import logo from '../assets/images/logoLogicQuest.png';
+import {Container, Row, Col} from 'react-bootstrap';
+import { BsInstagram, BsLinkedin, BsFacebook } from "react-icons/bs";
 
-function Footer() {
+function Footer(){
     return (
         <div id="footer">
-            <p>Footer</p>
+            <Container>
+                <Row>
+                    <Col>
+                        <img className="imageFooter" src={logo} alt="Logo"/>
+                    </Col>
+                    <Col>
+                        <div className="iconsFooter">
+                            <a href='https://www.facebook.com/drdbusinessbr?mibextid=LQQJ4d'>
+                                <BsFacebook className='iconFooter'/>
+                            </a>
+                            <a href='https://instagram.com/drd.business?igshid=YmMyMTA2M2Y='>
+                                <BsInstagram className='iconFooter'/>
+                            </a>
+                            <a href='/'>
+                                <BsLinkedin className='iconFooter'/>
+                            </a>                       
+                        </div>
+                        
+                    </Col> 
+                </Row>
+            </Container>
         </div>
     )
 }
