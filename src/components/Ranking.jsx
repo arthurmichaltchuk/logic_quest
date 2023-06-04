b0b7cdbbd8cc46b1cb607cd3229f41bcf7a32c4c
AugustoJr01 <augustovelho2012.avf@gmail.com>
2023-06-04 02:07:27 -0300

    Feito Ranking

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,34 +1,37 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
-import img1 from '../assets/images/logoLogicQuest.png'
+import RankingImg from '../assets/images/RankingImg.png'
+import medalhaOuro from "../assets/images/medalhaOuro.png"
+import medalhaPrata from "../assets/images/medalhaPrata.png"
+import medalhaBronze from "../assets/images/medalhaBronze.png"
 
 function Ranking() {
     return (
         <div className="fundo pb-5" style={{ alignItems: 'flex-end', display: 'flex' }}>
             <Container>
-                <h1 className="text-center pt-5 pb-5">Ranking</h1>
+                <img className="ranking" src={RankingImg} alt="ranking" />
                 <div className="rankingBody text-center">
-                    <h4 className="mb-5">4 / 6</h4>
+                    <h4 className="mb-4">Acertou: 4 / 6</h4>
                     <Row>
-                        <Col style={{ padding: '0px' }}>
-                            <h1 style={{ marginTop: '80px' }}>ARTHUR</h1>
+                        <Col className="backRanking" style={{ margin: '60px 0 0 0'}} >
+                            <h1 style={{ marginTop: '20px' }}>Carol</h1>
                             <div className="Card rankingCard1">
-                                <img className="mt-5" height={180} src={img1} alt="Img"/>
-                                <h2 className="pt-5">Pontuação</h2>
+                                <img className="mt-2" height={150} src={medalhaPrata} alt="Img" />
+                                <h2 className="pt-5">60 pontos</h2>
                             </div>
                         </Col>
-                        <Col style={{ padding: '0px' }}>
-                            <h1>ARTHUR</h1>
+                        <Col className="backRanking" style={{ padding: '15px 0 0 0' }}>
+                            <h1 style={{ marginTop: '5px' }}>Arthur</h1>
                             <div className="Card rankingCard2">
-                                <img className="mt-5" height={180} src={img1} alt="Img"/>
-                                <h2 className="pt-5">Pontuação</h2>
+                                <img className="mt-2" height={150} src={medalhaOuro} alt="Img" />
+                                <h2 className="pt-5">90 pontos</h2>
                             </div>
                         </Col>
-                        <Col style={{ padding: '0px' }}>
-                            <h1 style={{ marginTop: '180px' }}>ARTHUR</h1>
+                        <Col className="backRanking" style={{ margin: '150px 0 0 0' }}>
+                            <h1 style={{ marginTop: '20px' }}>Augusto</h1>
                             <div className="Card rankingCard3">
-                                <img className="mt-5" height={180} src={img1} alt="Img"/>
-                                <h2 className="pt-5">Pontuação</h2>
+                                <img className="mt-2" height={150} src={medalhaBronze} alt="Img" />
+                                <h2 className="pt-5">20 pontos</h2>
                             </div>
                         </Col>
                     </Row>
